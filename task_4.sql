dda0fd791d8f5eb902bb0e6e6f0b6284c73e8e24
ebubeikechi <ebubeikechi@gmail.com>
2025-10-25 20:37:29 +0100

    modified task 4 script

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,17 @@
 USE alx_book_store;
 SHOW CREATE TABLE books;
+-- task_4.sql
+-- Displays the full description of the 'books' table
+-- without using DESCRIBE or EXPLAIN
 
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    COLUMN_KEY,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
  
